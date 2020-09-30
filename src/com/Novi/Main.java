4e03c3fe3747616dc548db1cfcfb2ba8a1464d08
Your Name <j.hamwijk34@gmail.com>
2020-09-30 09:09:45 +0200

    Update Main.java

--- src/com/Novi/Main.java
+++ src/com/Novi/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // handle exception
+            // only allow numbers
+            // no symbols
+            // no chars
+
+        // if else loop
+        // break
+        // structure
+        // unit test
+        
     }
 }
